bc9e0f95039d728e2f56076610c706bc732efc6d
alpha7179 <alpha_loading7179@naver.com>
2025-06-09 14:05:23 +0900

    [Chore] 미션2 빈테이불시 타켓레이어 활성화 메소드 추가

--- Assets/BMW/Scripts/Mission2TargetManager.cs
+++ Assets/BMW/Scripts/Mission2TargetManager.cs
@@ -1,18 +1,48 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Mission2TargetManager : MonoBehaviour
 {
-    [Header("CustomLayer")]
-    public LayerMask customLayerMask;
+    [Header("Layer")]
+    [SerializeField] private LayerMask customLayerMask;
+    [SerializeField] private LayerMask defaultLayerMask;
+    [Header("DeBug Log")]
+    [SerializeField] private bool IsDebug = false;
 
-    [Header("DefaultLayer")]
-    public LayerMask defaultLayerMask;
+    private int _cutleryCount = 0;
+    private List<string> _enteredObjects = new List<string>();
+    private Dictionary<string, int> _previousLayers = new Dictionary<string, int>();
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Cutlery"))
         {
-            other.gameObject.layer = Mathf.RoundToInt(Mathf.Log(defaultLayerMask.value, 2));
+            string objName = other.gameObject.name;
+
+            if (!_enteredObjects.Contains(objName))
+            {
+                _previousLayers[objName] = other.gameObject.layer;
+
+                _enteredObjects.Add(objName);
+                _cutleryCount++;
+
+                int defaultLayerIndex = Mathf.RoundToInt(Mathf.Log(defaultLayerMask.value, 2));
+                other.gameObject.layer = defaultLayerIndex;
+
+                if (IsDebug)
+                    Debug.LogWarning($"[Enter] Added: {objName} | Total: {_cutleryCount}");
+
+                if (other.gameObject.layer != _previousLayers[objName] && IsDebug)
+                {
+                    Debug.LogWarning($"[Layer Changed] {objName}: " +
+                        $"{LayerMask.LayerToName(_previousLayers[objName])} ¡æ " +
+                        $"{LayerMask.LayerToName(other.gameObject.layer)}");
+                }
+            }
+            else if (IsDebug)
+            {
+                Debug.LogWarning($"[Enter Ignored] Duplicate: {objName}");
+            }
         }
     }
 
@@ -20,7 +50,38 @@ public class Mission2TargetManager : MonoBehaviour
     {
         if (other.CompareTag("Cutlery"))
         {
-            other.gameObject.layer = Mathf.RoundToInt(Mathf.Log(customLayerMask.value, 2));
+            string objName = other.gameObject.name;
+
+            if (_enteredObjects.Contains(objName))
+            {
+                int prevLayer = other.gameObject.layer;
+
+                _enteredObjects.Remove(objName);
+                _cutleryCount = Mathf.Max(0, _cutleryCount - 1);
+
+                if (IsDebug)
+                    Debug.LogWarning($"[Exit] Removed: {objName} | Remaining: {_cutleryCount}");
+
+                if (_cutleryCount <= 0)
+                {
+                    int customLayerIndex = Mathf.RoundToInt(Mathf.Log(customLayerMask.value, 2));
+                    other.gameObject.layer = customLayerIndex;
+                    _cutleryCount = 0;
+
+                    if (other.gameObject.layer != prevLayer && IsDebug)
+                    {
+                        Debug.LogWarning($"[Layer Changed] {objName}: " +
+                            $"{LayerMask.LayerToName(prevLayer)} ¡æ " +
+                            $"{LayerMask.LayerToName(other.gameObject.layer)}");
+                    }
+                }
+
+                _previousLayers.Remove(objName);
+            }
+            else if (IsDebug)
+            {
+                Debug.LogWarning($"[Exit Ignored] Not in list: {objName}");
+            }
         }
     }
 }
